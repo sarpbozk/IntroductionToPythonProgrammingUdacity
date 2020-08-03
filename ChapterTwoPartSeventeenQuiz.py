2822929072b163a983453301af4b58c80e9dfcc9
Muhammet Bozkurt <mubozk@icloud.com>
2020-08-03 12:07:58 +0300

    commit

---
+++ ChapterTwoPartSeventeenQuiz.py
@@ -0,0 +1,26 @@
+#first
+#mon_sales = "121"
+#tues_sales = "105"
+#wed_sales = "110"
+#thurs_sales = "98"
+#fri_sales = "95"
+#total_sales = 
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+
+mon_sales = "121"
+mon_sales = int(mon_sales)
+tues_sales = "105"
+tues_sales = int(tues_sales)
+wed_sales = "110"
+wed_sales = int(wed_sales)
+thurs_sales = "98"
+thurs_sales = int(thurs_sales)
+fri_sales = "95"
+fri_sales = int(fri_sales)
+
+total_sales = mon_sales + tues_sales + wed_sales + thurs_sales + fri_sales
+total_sales = str(total_sales)
+print(total_sales + "\n" )
+#########
